d448ab3e56ead6ca49d40acebe89cf1d951cdd34
Ahmet Ali Aşık <ahmethilmiask@gmail.com>
2026-01-02 13:33:00 +0300

    Added minsdk version compatible for minsdkversion 31 to 21

--- app/src/main/java/com/ahmetaliasik/signalpowerdetector/core/telephonyManager/SignalStrengthObserver.kt
+++ app/src/main/java/com/ahmetaliasik/signalpowerdetector/core/telephonyManager/SignalStrengthObserver.kt
@@ -1,13 +1,16 @@
 package com.ahmetaliasik.signalpowerdetector.core.telephonyManager
 
 import android.content.Context
+import android.os.Build
 import android.telephony.CellSignalStrengthGsm
 import android.telephony.CellSignalStrengthLte
 import android.telephony.CellSignalStrengthNr
 import android.telephony.CellSignalStrengthWcdma
+import android.telephony.PhoneStateListener
 import android.telephony.SignalStrength
 import android.telephony.TelephonyCallback
 import android.telephony.TelephonyManager
+import androidx.annotation.RequiresApi
 
 class SignalStrengthObserver(
     private val context: Context,
@@ -16,14 +19,33 @@ class SignalStrengthObserver(
     private val telephonyManager =
         context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
 
-    private val callback =
-        object : TelephonyCallback(), TelephonyCallback.SignalStrengthsListener {
+    private var modernCallback : Any? = null
 
+    private var legacyListener : PhoneStateListener? = null
+
+    fun start() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            startModernListening()
+        } else {
+            startLegacyListening()
+        }
+    }
+
+    fun stop() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            stopModernListening()
+        } else {
+            stopLegacyListening()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun startModernListening(){
+        val callBack = object : TelephonyCallback(), TelephonyCallback.SignalStrengthsListener {
             override fun onSignalStrengthsChanged(signalStrength: SignalStrength) {
-
                 val strength = signalStrength.cellSignalStrengths.firstOrNull()
 
-                val dbm = when (strength) {
+                val dbm = when(strength) {
                     is CellSignalStrengthLte -> strength.rsrp
                     is CellSignalStrengthNr -> strength.ssRsrp
                     is CellSignalStrengthWcdma -> strength.dbm
@@ -39,18 +61,50 @@ class SignalStrengthObserver(
 
         }
 
-    fun start() {
+        modernCallback = callBack
+
         telephonyManager.registerTelephonyCallback(
             context.mainExecutor,
-            callback
+            callBack
         )
     }
-
-    fun stop() {
-        telephonyManager.unregisterTelephonyCallback(callback)
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun stopModernListening(){
+        modernCallback?.let {
+            if (it is TelephonyCallback) {
+                telephonyManager.unregisterTelephonyCallback(it)
+            }
+        }
+        modernCallback = null
     }
 
-    fun rsrpToPercent(rsrp: Int): Int {
+    @Suppress("DEPRECATION")
+    private fun startLegacyListening() {
+        legacyListener = object : PhoneStateListener() {
+            override fun onSignalStrengthsChanged(signalStrength: SignalStrength?) {
+                super.onSignalStrengthsChanged(signalStrength)
+                signalStrength?.let {
+                    val level = it.level
+                    val percent = ((level * 100) / 4).coerceIn(0, 100)
+                    onSignalChanged(percent)
+                }
+            }
+        }
+
+        telephonyManager.listen(
+            legacyListener,
+            PhoneStateListener.LISTEN_SIGNAL_STRENGTHS
+        )
+    }
+    @Suppress("DEPRECATION")
+    private fun stopLegacyListening() {
+        legacyListener?.let {
+            telephonyManager.listen(it, PhoneStateListener.LISTEN_NONE)
+        }
+        legacyListener = null
+    }
+
+    private fun rsrpToPercent(rsrp: Int): Int {
         return ((rsrp + 120) * 100 / 40).coerceIn(0, 100)
     }
 
